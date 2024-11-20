c48f7e2b24e09ba6adfee71c44b1043974760113
ulfhiraa <studyiraa@gmail.com>
2024-11-20 18:08:46 +0700

    fungsi saveDataKRS untuk menyimpan data MK dan Kelas pada rencana study

--- app/src/main/java/com/example/pam_meet8/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pam_meet8/ui/viewmodel/RencanaStudyViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.pam_meet8.ui.viewmodel
 
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.lifecycle.ViewModel
 import com.example.pam_meet8.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -12,7 +13,7 @@ class RencanaStudyViewModel : ViewModel(){ // class turunan dari View Model
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow() // data bersifat Read-Only
 
     fun setMataKuliah(mkPilihan: String){
-        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
     // update untuk mengubah nilai MK yang ada di _krsState
     // _krsState.update memastikan bahwa perubahan nilai namaMK disalurkan ke _krsState
     // copy untuk membuat salinan dari objek RencanaStudy dan mengganti nilai namaMK dengan mkPilihan yang diberikan
@@ -22,4 +23,12 @@ class RencanaStudyViewModel : ViewModel(){ // class turunan dari View Model
     fun setKelas(kelasPIlihan: String){
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPIlihan) }
     } // fungsi untuk memperbarui pilihan kelas
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls[1]
+        )
+        }
+    } // fungsi untuk menyimpan data MK dan Kelas pada rencana study
 }
