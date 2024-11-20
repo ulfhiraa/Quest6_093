a6361d161f415563dd3b0c9435d1c27f667fbda1
ulfhiraa <studyiraa@gmail.com>
2024-11-20 18:00:37 +0700

    fungsi setMataKuliah untuk memperbarui nama MK dalam RencanaStudy dengan nilai yang diberikan mkPilihan

--- app/src/main/java/com/example/pam_meet8/ui/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pam_meet8/ui/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,13 @@ import com.example.pam_meet8.model.RencanaStudy
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel(){ // class turunan dari View Model
     private val _krsState = MutableStateFlow(RencanaStudy())
     val krsStateUi: StateFlow<RencanaStudy> = _krsState.asStateFlow() // data bersifat Read-Only
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
 }
