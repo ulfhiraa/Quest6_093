e4532ef4486b710177b51ebb1cbeea22459dc339
ulfhiraa <studyiraa@gmail.com>
2024-11-20 16:20:46 +0700

    objek untuk menyimpan data ruang kelas yang dipilih dengan radio button

--- app/src/main/java/com/example/pam_meet8/data/RuangKelas.kt
+++ app/src/main/java/com/example/pam_meet8/data/RuangKelas.kt
@@ -1,10 +1,11 @@
 package com.example.pam_meet8.data
 
 object RuangKelas {
-    val listKelas= listOf(
+    val lisKelas= listOf(
         "A",
         "B",
         "C",
         "D",
     )
-} // objek ruang kelas yang dapat dipilih dengan radio button
+}
+// objek ruang kelas yang dapat dipilih dengan radio button
