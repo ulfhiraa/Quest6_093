486d2c8a912a0fff6beb66e069684ba3c96b8b22
ulfhiraa <studyiraa@gmail.com>
2024-11-20 16:52:00 +0700

    membuat row dan menambahkan komponen image untuk gambar umy dan menambahkan spacer untuk mengatur jaraknya

--- app/src/main/java/com/example/pam_meet8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_meet8/ui/screen/RencanaStudyView.kt
@@ -1,16 +1,28 @@
 package com.example.pam_meet8.ui.screen
 
 import android.os.strictmode.UntaggedSocketViolation
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.pam_meet8.R
 import com.example.pam_meet8.model.Mahasiswa
 import com.example.pam_meet8.model.RencanaStudy
@@ -36,6 +48,20 @@ fun RencanaStudyView(
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ){
-
+        Row (
+            modifier = Modifier
+                .fillMaxWidth() // agar row menjadi selebar layar
+                .padding(16.dp), // jarak 16dp diseluruh sisi Row
+            verticalAlignment = Alignment.CenterVertically // mengatur agar row berada ditengah
+        ){
+            Image( // komponen gambar
+                painter = painterResource(id = R.drawable.umy), // sumber gambar dari res drawable.umy
+                contentDescription = "", // deskripsi konten null
+                modifier = Modifier
+                    .clip(shape = CircleShape) // membuat gambar menjadi lingkaran
+                    .size(50.dp) // ukuran gambar 50dp
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+        }
     }
 }
