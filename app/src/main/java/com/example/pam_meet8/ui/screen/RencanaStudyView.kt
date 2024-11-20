b4710b57f36c4cda66de21c8534ef629b9f7ef4b
ulfhiraa <studyiraa@gmail.com>
2024-11-20 16:40:00 +0700

    mengatur modifier untuk size dan background warna

--- app/src/main/java/com/example/pam_meet8/ui/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/pam_meet8/ui/screen/RencanaStudyView.kt
@@ -1,11 +1,17 @@
 package com.example.pam_meet8.ui.screen
 
 import android.os.strictmode.UntaggedSocketViolation
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.pam_meet8.R
 import com.example.pam_meet8.model.Mahasiswa
 import com.example.pam_meet8.model.RencanaStudy
 
@@ -25,5 +31,11 @@ fun RencanaStudyView(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas) // menampilkan data pilihan kelas
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
 
+    }
 }
